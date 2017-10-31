81c0580c5f7075d629e25af6a34d748dc68e73eb
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2017-10-31 17:39:53 -0400

    split batch wrappers

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const { DynamoDB } = require('aws-sdk');
 const wrapped = require('./lib/wrapped');
 const paginated = require('./lib/paginated');
-const batched = require('./lib/batched');
+const batchGetItem = require('./lib/batch_get_item');
+const batchWriteItem = require('./lib/batch_write_item');
 
 function Dynaflow(options) {
   if (new.target !== Dynaflow) return new Dynaflow(options);
   this.db = new DynamoDB(options);
   wrapped(this);
   paginated(this);
-  batched(this);
+  batchGetItem(this);
+  batchWriteItem(this);
 }
 
 module.exports = Dynaflow;
