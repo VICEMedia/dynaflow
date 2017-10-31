bae0b90fe5805e0d4e3403e443cefff628725f1a
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2017-10-31 16:19:08 -0400

    added boilerplate

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const { DynamoDB } = require('aws-sdk');
 const wrapped = require('./lib/wrapped');
 const paginated = require('./lib/paginated');
+const batched = require('./lib/batched');
 
 function Dynaflow(options) {
   if (new.target !== Dynaflow) return new Dynaflow(options);
   this.db = new DynamoDB(options);
   wrapped(this);
   paginated(this);
+  batched(this);
 }
 
 module.exports = Dynaflow;
