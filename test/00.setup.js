064f18848e3ba2d198438f78b21925281d7af99b
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2017-11-02 15:34:25 -0400

    added test folder

---
+++ test/00.setup.js
@@ -0,0 +1,2 @@
+'use strict';
+require('chai').use(require('chai-as-promised'));
---
+++ test/10.table_methods.js
@@ -0,0 +1,8 @@
+'use strict';
+const { expect } = require('chai');
+
+describe('createTable()', function () {
+  it('should be tested', function () {
+    expect(true).to.be.ok;
+  });
+});
