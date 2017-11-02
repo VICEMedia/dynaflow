6a564e81b45c23403ca48b8699abb6c62579e74a
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2017-11-02 15:04:04 -0400

    added dummy test

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const wrapped = require('./lib/wrapped');
 const paginated = require('./lib/paginated');
 const batchWriteItem = require('./lib/batch_write_item');
 
+/*
+  The methods found on instances of DynamoDB are not available at
+  DynamoDB.prototype, so we have to do our bootstrapping on a per-instance basis.
+ */
+
 function Dynaflow(options) {
   if (new.target !== Dynaflow) return new Dynaflow(options);
   this.db = new DynamoDB(options);
-  wrapped(this);
-  paginated(this);
-  batchWriteItem(this);
+  wrapped(this); // Attach promise-returning methods
+  paginated(this); // Attach river-returning methods
+  batchWriteItem(this); // Attach agent-returning method
 }
 
 module.exports = Dynaflow;
