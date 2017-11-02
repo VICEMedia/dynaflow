d0225c41cced5152bbe4805f3cd826ec54cd7112
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2017-11-02 14:47:42 -0400

    implemented batch writer

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const { DynamoDB } = require('aws-sdk');
 const wrapped = require('./lib/wrapped');
 const paginated = require('./lib/paginated');
-const batchGetItem = require('./lib/batch_get_item');
 const batchWriteItem = require('./lib/batch_write_item');
 
 function Dynaflow(options) {
@@ -10,7 +9,6 @@ function Dynaflow(options) {
   this.db = new DynamoDB(options);
   wrapped(this);
   paginated(this);
-  batchGetItem(this);
   batchWriteItem(this);
 }
 
